db6aa815594fcc2b843c95d439bc99926ccf8c22
Itay Barok <itay.barok@gmail.com>
2022-02-05 22:57:32 +0200

    changed to the for the submit

---
+++ CodeGuroCompetitionZombies/trapZombies/solve_zombies_v1.py
@@ -0,0 +1,37 @@
+
+def encrypt_ax(ax, cx=0x35, bx=0X11BF):
+    ax = ax ^ 0xFAEB
+    temp = ax
+
+    for j in range(10000):
+        for i in range(cx):
+            ax = ax * bx
+            ax = ax & 0xffff  # ax is 32 bits
+            ax = ax + 0x4743
+            ax = ax & 0xffff  # ax is 32 bits
+
+        if temp & 0xff == ax & 0xff:
+            print(f"INDEX: {j+1}")
+            break
+
+    #print(hex(ax & 0xffff))
+
+encrypt_ax(0x317) # 00[XY] -- FF[XY]
+
+val = 0xfaeb
+
+
+def decrypt_ax(encrypted_ax, cx, bx):
+    """
+        reverse the encryption of ax
+    """
+
+    for i in range(cx):
+        encrypted_ax = encrypted_ax - 0x4743
+        encrypted_ax = encrypted_ax // bx
+
+    print(hex(encrypted_ax & 0xffff))
+
+    print(hex(( ((encrypted_ax & 0xFFFF) - 0x4743) // 0x11bf ) ^ 0xfaeb ))
+
+
---
+++ CodeGuroCompetitionZombies/trapZombies/trapZombA_v2_no_decoy.asm
@@ -0,0 +1,45 @@
+%define PREFIX_MOV_OPCODE 0xB8
+%define JMP_AX_OPCODE 0xE0FF
+%define JMP_MEM_OPCODE 0X26FF
+
+mov cx, ax
+ 
+repeat:   
+    mov ax, [0xb60f] ; hex(0xb111 + n * 0x4FE + 2 * (n - 1))
+    cmp ax, 0xCCCC
+    jne got_result
+    jmp repeat
+    
+ 
+got_result:
+    mov bx, 0xe33f
+    mul word bx
+    add ax, 0x2503
+    
+    xor ax, 0xFAEB
+    
+ 
+trapZombie:
+    xchg cx, ax
+    mov bx, ax
+    add ax, zombieTrap
+    lea bx, [bx + end + 2]
+    mov [bx], ax
+    mov ax, bx 
+
+    mov bx, cx
+    add bx, 19
+    
+    mov word [bx], ax
+    mov word [bx - 2], JMP_MEM_OPCODE
+        
+    jmp bomb
+  
+bomb:
+   jmp $
+   
+zombieTrap:
+   jmp $
+  
+  
+end:
